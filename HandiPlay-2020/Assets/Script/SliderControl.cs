c4346dbcb9346d18ad667191089a9778375f6ba7
Yovador <yovador@gmail.com>
2020-04-17 17:46:55 +0200

    fin de journée

---
+++ HandiPlay-2020/Assets/Script/SliderControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderControl : MonoBehaviour
+{
+    private Slider slider;
+    public string settingsToChange;
+
+    private void Start()
+    {
+        slider = GetComponent<Slider>();
+        if (settingsToChange == "GameSpeed")
+        {
+            SetDefaultSlider("GameSpeed");
+        }
+
+    }
+
+    private void Update()
+    {
+        if (settingsToChange == "GameSpeed")
+        {
+            ChangeValue("GameSpeed");
+        }
+    }
+
+    private void ChangeValue(string key)
+    {
+        
+        if (PlayerPrefs.GetFloat(key) != slider.value)
+        {
+            PlayerPrefs.SetFloat(key, slider.value);
+        }
+
+    }
+
+    private void SetDefaultSlider(string key)
+    {
+        slider.value = PlayerPrefs.GetFloat(key);
+    }
+}
